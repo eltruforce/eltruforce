54515e7c8af3fa8fd50bf53306af940b1637195a
eltruforce <lordace550@gmail.com>
2022-07-13 20:49:49 -0500

    feat: initial configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,32 @@
+import { useEffect } from "react";
+import { Scene, WebGL1Renderer, PerspectiveCamera } from "three";
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  useEffect(() => {
+    const scene = new Scene();
+    const renderer = new WebGL1Renderer({
+      antialias: true,
+      canvas: document.getElementById("bg"),
+    });
+    const camera = new PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    // move camera
+    camera.position.z = 6;
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    function animate() {
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    }
+    animate();
+  }, []);
+  return <canvas id="bg" />;
 }
 
 export default HomePage;
